cfdd4c1ec3e9b1995a40f1a7682f5dd7e6a61e39
Amber Ramesh <amramesh@iu.edu>
2022-02-01 03:07:30 -0500

    (#1) Add configuration files and axios - Add env files for dev and production - Add axios for XHR, qs for param serialization - Set app title and server port

---
+++ weather-forecast-ui/src/api/index.js
@@ -0,0 +1,9 @@
+import axios from 'axios'
+import qs from 'qs'
+
+const instance = axios.create({
+  baseURL: process.env.VUE_APP_WEB_API_BASE_URL,
+  paramsSerializer: params => qs.stringify(params, { arrayFormat: 'repeat', allowDots: true })
+})
+
+export default instance
--- weather-forecast-ui/vue.config.js
+++ weather-forecast-ui/vue.config.js
@@ -1,5 +1,16 @@
 module.exports = {
+  chainWebpack: config => {
+    config
+      .plugin('html')
+      .tap(args => {
+        args[0].title = process.env.VUE_APP_TITLE
+        return args
+      })
+  },
   transpileDependencies: [
-    'vuetify',
+    'vuetify'
   ],
-};
+  devServer: {
+    port: 8085
+  }
+}
