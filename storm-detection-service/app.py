4505ed4b2cf46cd0bd3a8d6829de9b6a9c07d962
akshace <51332268+Akshace@users.noreply.github.com>
2022-02-01 22:51:25 -0500

    Pushing code for #10 to feature branch. Needs Kafka broker.

---
+++ storm-detection-service/app.py
@@ -0,0 +1,73 @@
+from distutils.log import debug
+from flask import Flask, request
+# from nexrad import NexRad
+from pykafka import KafkaClient
+from pykafka.common import OffsetType
+import json
+# from urllib.parse import urlparse
+from pykml.factory import KML_ElementMaker as KML
+from pykml.factory import GX_ElementMaker as GX
+from lxml import etree
+import json
+
+# import nexradaws
+app = Flask(__name__)
+# app.config.from_pyfile('config.py')
+
+
+app.config.from_pyfile('config.py')
+kafka_server = app.config['KAFKA_SERVER']
+kafka_server_port = app.config['KAFKA_SERVER_PORT']
+
+client = KafkaClient(hosts=f'{kafka_server}:{kafka_server_port}')
+
+
+@app.route('/')
+def index():
+    return 'Storm Detection Service'
+
+
+def generate_kml(self, data):
+    """Read data from ingestor service and parse paramaters and generate KML for storm data"""
+
+    """Using Dummy KML to mock data for Assignment 1"""
+
+    pm1 = KML.Placemark(
+             KML.name("Storm brewing.."),
+             KML.Point(
+               KML.coordinates("-88.23999786376953,30.679445266723633")
+             )
+          )
+          
+    pm2 = KML.Placemark(
+             KML.name("Storm... "),
+             KML.Point(
+               KML.coordinates("-88.24999786376953,30.699445266723633")
+             )
+           )
+    pm3 = KML.Placemark(
+             KML.name("Storm Alert...!"),
+             KML.Point(
+               KML.coordinates("-88.25999786376953,30.679445266723633")
+             )
+           )
+    fld = KML.Folder(pm1,pm2)
+    fld2= KML.Folder(fld,pm3)
+    stri = etree.tostring(fld2, pretty_print=True)
+    topic = client.topics[app.config['KAFKA_PRODUCER_TOPIC']]
+    with topic.get_producer(delivery_reports = False) as producer:
+      producer.produce(json(stri))
+    # outfile = open('pls_work_now1.kml','wb')
+    # outfile.write(stri)
+
+
+if __name__ == "__main__": 
+  # app.run(debug= True) 
+  messages = client.topics[app.config['KAFKA_CONSUMER_TOPIC']].get_simple_consumer(
+            auto_offset_reset=OffsetType.LATEST,
+            reset_offset_on_start=True)
+    
+  for message in messages:
+    if message is not None:
+      data = json.loads(message.value.decode('utf-8'))
+      generate_kml(data)
---
+++ storm-detection-service/config.py
@@ -0,0 +1,6 @@
+"""FLASK CONFIGURATIONS"""
+
+KAFKA_SERVER = 'localhost'
+KAFKA_SERVER_PORT = '9092'
+KAFKA_CONSUMER_TOPIC = 'storm-detection'
+KAFKA_PRODUCER_TOPIC = 'storm-clustering'
