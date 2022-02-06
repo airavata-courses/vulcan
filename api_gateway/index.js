7bc5e4e75828a3fbaff179ce31db49ff17e29754
akshace <51332268+Akshace@users.noreply.github.com>
2022-02-05 21:07:44 -0500

    Pushing passport-jwt auth #5

---
+++ api_gateway/index.js
@@ -0,0 +1,57 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const passport = require('passport')
+const bodyParser = require('body-parser')
+const User = require('./Models/User')
+const jwt = require('jsonwebtoken');
+require('./passport')
+const app = express()
+genToken = user => {
+  return jwt.sign({
+    iss: 'Joan_Louji',
+    sub: user.id,
+    iat: new Date().getTime(),
+    exp: new Date().setDate(new Date().getDate() + 1)
+  }, 'joanlouji');
+}
+app.use(bodyParser.json())
+app.get('/',(req,res)=>{
+  res.send('Hello world')
+})
+
+// app.get('/secret',passport.authenticate('jwt',{session:false})),
+// (req,res,next) => {
+//     res.json('Secret Data')
+// }
+
+
+app.post('/register', async function (req, res, next) {
+  const { email, password } = req.body;
+  
+  //Check If User Exists
+  let foundUser = await User.findOne({ email });
+  if (foundUser) {
+    return res.status(403).json({ error: 'Email is already in use'});
+  }
+ 
+  const newUser = new User({ email, password})
+  await newUser.save()
+  // Generate JWT token
+  const token = genToken(newUser)
+  res.status(200).json({token})
+});
+mongoose.connect("mongodb://localhost/27017", {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connection.once('open',function(){
+  console.log('Connected to Mongo');
+}).on('error',function(err){
+  console.log('Mongo Error', err);
+})
+
+
+app.get('/secret', passport.authenticate('jwt',{session: false}),(req,res,next)=>{
+    res.json("Secret Data")
+})
+
+app.listen(8000,()=>{
+  console.log('Serve is up and running at the port 8000')
+})
