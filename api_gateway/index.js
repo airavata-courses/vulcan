5ded3b97be79ecac4cd4a4e7a579b4cdeb71a1e0
Amber Ramesh <amramesh@iu.edu>
2022-02-07 21:43:05 -0500

    (#5) Fix issue with kafka and login - Install CORS plugin

--- api_gateway/index.js
+++ api_gateway/index.js
@@ -7,8 +7,10 @@ const jwt = require('jsonwebtoken');
 require('./passport')
 const app = express()
 const server = require('http').createServer(app);
+const cors = require('cors')
 const WebSocket = require('ws');
-const wss = new WebSocket.Server({server});
+const wss = new WebSocket.Server({ server });
+app.use(cors());
 
 genToken = user => {
   return jwt.sign({
@@ -19,49 +21,35 @@ genToken = user => {
   }, 'joanlouji');
 }
 app.use(bodyParser.json())
-app.get('/',(req,res)=>{
-  res.send('Hello world')
-})
 
-// app.get('/secret',passport.authenticate('jwt',{session:false})),
-// (req,res,next) => {
-//     res.json('Secret Data')
-// }
-
-const consumer=require('./config/kafka-config').consumer
-const producer = require("./config/kafka-config").producer;
-const { json } = require('body-parser')
-const { error } = require('console')
+const consumer = require('./config/kafka-config').consumer
+const producer = require("./config/kafka-config").producer
 
 // This can go inside the post request when user makes the data request or when login is authenticated.
-wss.on('connection', function connection(ws){
+wss.on('connection', function connection(ws) {
   console.log('A new client Connection!')
-  ws.send('Welcome new Client');
+  // ws.send('Welcome new Client');
   consumer.on("message", function (message) {
-    console.log(message);
-    // connection.sendUTF(message.value);
+    console.log('received: %s', message.value);
+    ws.send(message.value)
   })
 
-  ws.on('message', function incoming(message){
-    msg = JSON.stringify(message)
-
+  ws.on('message', function incoming(message) {
+    msg = new TextDecoder('utf-8').decode(new Uint8Array(message))
+    console.log(msg)
     let payloads = [
       {
-        topic : "gateway",
-        messages : msg
+        topic: "ingestor",
+        messages: msg
       }];
-    
-    producer.on('ready', function () {
-      producer.send(payloads, function (err, data) {
-          console.log(data);
-      });
+
+    producer.send(payloads, function (err, data) {
+      console.error(err);
     });
 
-    producer.on('error', function (err) {})
-
-    console.log('received: %s', message);
-
-    ws.send('Got your message its :' + message)
+    producer.on('error', function (err) {
+      console.error(err)
+    })
   });
 });
 
@@ -70,46 +58,41 @@ wss.on('connection', function connection(ws){
 
 app.post('/register', async function (req, res, next) {
   const { email, password } = req.body;
-  
+
   //Check If User Exists
   let foundUser = await User.findOne({ email });
   if (foundUser) {
-    return res.status(403).json({ error: 'Email is already in use'});
+    return res.status(403).json({ error: 'Email is already in use' });
   }
- 
-  const newUser = new User({ email, password})
+
+  const newUser = new User({ email, password })
   await newUser.save()
   // Generate JWT token
   const token = genToken(newUser)
-  res.status(200).json({token})
+  res.status(200).json({ token })
 });
 
 
-mongoose.connect("mongodb://localhost/27017", {useNewUrlParser: true, useUnifiedTopology: true});
-mongoose.connection.once('open',function(){
+mongoose.connect("mongodb://localhost/27017", { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connection.once('open', function () {
   console.log('Connected to Mongo');
-}).on('error',function(err){
+}).on('error', function (err) {
   console.log('Mongo Error', err);
 })
 
 
-app.post('/login',passport.authenticate('jwt',{session:false}), (req, res, next) => {
+app.post('/login', passport.authenticate('jwt', { session: false }), (req, res, next) => {
   const { email, password } = req.body;
-  let foundUser = User.findOne({ email });
+  let foundUser = await User.findOne({ email, password });
   if (foundUser) {
     return res.status(200).json("Login Succesful");
   }
   return res.status(403).json("Invalid User Name")
 })
 
-app.get('/secret', passport.authenticate('jwt',{session: false}),(req,res,next)=>{
-    res.json("Secret Data")
+app.get('/secret', passport.authenticate('jwt', { session: false }), (req, res, next) => {
+  res.json("Secret Data")
 })
 
 
 server.listen(3000, () => console.log('Listenining on port : 3000'))
-
-// let config = require("./config/config");
-// app.listen(8000,()=>{
-//   console.log('Server is up and running at the port 8000')
-// })
