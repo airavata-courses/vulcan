b235a82a42ec116762689813b9cf4817761cd7e8
ratchahan-anbarasan <ratchahan9897@gmail.com>
2022-02-15 23:52:43 -0500

    Implementation for Login API and bcrypt based password encryption

--- user-management/src/main/java/com/Weather365/usermanagement/service/userService.java
+++ user-management/src/main/java/com/Weather365/usermanagement/service/userService.java
@@ -3,6 +3,7 @@ package com.Weather365.usermanagement.service;
 import com.Weather365.usermanagement.model.user;
 import com.Weather365.usermanagement.repository.userRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,6 +15,8 @@ public class userService {
     public Integer register(user user){
 
         try {
+            String hashPassword = new BCryptPasswordEncoder().encode(user.getPassword());
+            user.setPassword(hashPassword);
             return this.repository.save(user).getUserId();
         }
         catch (Exception ex){
@@ -21,4 +24,20 @@ public class userService {
             return -1;
         }
     }
+
+    public boolean login(String emailID, String password) {
+
+        boolean retVal = false;
+        try {
+            var user = this.repository.findByEmailId(emailID);
+
+            retVal = new BCryptPasswordEncoder().matches(password, user.getPassword());
+//            retVal =  password.equals(user.getPassword());
+        } catch (Exception ex) {
+            System.out.println("Exception occured in user service - isValidUser" + ex);
+        }
+
+        return retVal;
+    }
+
 }
