949b854c50606c9d855b4471f9b619b833f510b2
ratchahan-anbarasan <ratchahan9897@gmail.com>
2022-02-24 22:46:50 -0500

    removed other service implementations

--- ingestor_service/run.sh
+++
@@ -1,5 +0,0 @@
-#install the pre-requisites
-pip3 install -r requirements.txt > /dev/null 2>&1
-
-#run the app
-python3 app.py
--- weather-forecast-ui/src/api/index.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-import qs from 'qs';
-
-const instance = axios.create({
-  baseURL: process.env.VUE_APP_WEB_API_BASE_URL,
-  paramsSerializer: (params) => qs.stringify(params, { arrayFormat: 'repeat', allowDots: true }),
-});
-
-export default instance;
