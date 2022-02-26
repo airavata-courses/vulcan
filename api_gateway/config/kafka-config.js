eb50838ee12258a9972ef4eb2b64a51eaacb95a4
akshace <51332268+Akshace@users.noreply.github.com>
2022-02-26 00:42:11 -0500

    Adding updated files for API Gateway #36

--- api_gateway/config/kafka-config.js
+++ api_gateway/config/kafka-config.js
@@ -1,22 +1,17 @@
 const kafka = require("kafka-node");
-// const Producer = kafka.Producer;
-// const Consumer = kafka.Consumer;
 
 var Consumer = kafka.Consumer,
  client = new kafka.KafkaClient("localhost:9092"),
  consumer = new Consumer(
- client, [ { topic: "gateway", partition: 0 } ], { autoCommit: false });
+ client, [ { topic: "gateway", partition: 0 }, 
+            {topic : "gateway-save", partition:1},
+            {topic : "gateway-get", partition : 2} ],
+             { autoCommit: false });
 
 
 var Producer = kafka.Producer,
-// KeyedMessage = kafka.KeyedMessage,
 client = new kafka.KafkaClient("localhost:9092"),
 producer = new Producer(client)
-
-// const ConsumerGroup = kafka.ConsumerGroup
-// const client = new kafka.KafkaClient({ kafkaHost: "kafka-service:9092" });
-// let config = require("./config");
-// producer = new Producer(client);
 
 
 module.exports = { producer, consumer };
