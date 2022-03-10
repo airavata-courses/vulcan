1c623b68a77db81382f8b9b295556c9a4ca99db6
ratchahan-anbarasan <ratchahan9897@gmail.com>
2022-03-09 21:46:54 -0500

    changed REST based implementation to Kafka streaming

---
+++ user-management/src/main/java/com/Weather365/usermanagement/configuration/Producer.java
@@ -0,0 +1,16 @@
+package com.Weather365.usermanagement.configuration;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class Producer {
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    public void send(String topic, String message){
+        kafkaTemplate.send(topic, message);
+    }
+}
--- user-management/src/main/java/com/Weather365/usermanagement/controller/userController.java
+++ user-management/src/main/java/com/Weather365/usermanagement/controller/userController.java
@@ -1,14 +1,18 @@
 package com.Weather365.usermanagement.controller;
 
+import com.Weather365.usermanagement.configuration.Producer;
 import com.Weather365.usermanagement.model.loginResponse;
 import com.Weather365.usermanagement.model.userRequest;
 import com.Weather365.usermanagement.model.userResponse;
 import com.Weather365.usermanagement.service.userService;
 import com.Weather365.usermanagement.utility.utility;
 import com.google.gson.reflect.TypeToken;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.web.bind.annotation.*;
 import com.google.gson.Gson;
 import java.lang.reflect.Type;
@@ -23,9 +27,17 @@ public class userController {
     @Autowired
     private utility utility;
 
-    @PostMapping
-    @RequestMapping(value = "/register")
-    public ResponseEntity<String> register(@RequestBody String user){
+    @Autowired
+    private Producer producer;
+
+    @Value("${topic.name.producer.user-register-response}")
+    private String registerResponse;
+
+    @Value ("${topic.name.producer.user-login-response}")
+    private String loginResponse;
+
+    @KafkaListener(topics = "${topic.name.consumer.user-register-request}", groupId = "group_id")
+    public void register(ConsumerRecord<String, String> registerRequest){
 
         //System-log
         System.out.println("Register API invoked");
@@ -34,15 +46,13 @@ public class userController {
         String _message = "Registration failed!";
         Gson gson = new Gson();
         String response = null;
-        HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
 
         try {
             Type modelType = new TypeToken<userRequest>() {}.getType();
-            userRequest data = gson.fromJson(user, modelType);
+            userRequest data = gson.fromJson(registerRequest.value(), modelType);
 
             if(this.service.getAccountByEmailId(data.getEmailId()) != null){
                 _message = "User already exists";
-                status = HttpStatus.OK;
             }
             else{
                 //validate the given user email address
@@ -50,7 +60,6 @@ public class userController {
                     if(this.service.register(data)){
                         _status = "success";
                         _message = "User Registration Success";
-                        status = HttpStatus.OK;
                         System.out.println(_message);
                     }
                 }
@@ -66,23 +75,21 @@ public class userController {
             System.out.println("Exception at register api " + ex);
         }
         response = gson.toJson(new userResponse(_status, _message));
-        return new ResponseEntity<>(response, status);
+        producer.send(registerResponse, response);
     }
 
-    @PostMapping
-    @RequestMapping(value = "/login")
-    public ResponseEntity<String> login(@RequestBody String loginRequest){
+    @KafkaListener(topics = "${topic.name.consumer.user-login-request}", groupId = "group_id")
+    public void login(ConsumerRecord<String, String> loginRequest){
 
         //System-log
         System.out.println("Login API invoked");
         String response = null;
-        HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
 
 
         try {
             Type modelType = new TypeToken<com.Weather365.usermanagement.model.loginRequest>() {}.getType();
             Gson gson = new Gson();
-            com.Weather365.usermanagement.model.loginRequest data = gson.fromJson(loginRequest, modelType);
+            com.Weather365.usermanagement.model.loginRequest data = gson.fromJson(loginRequest.value(), modelType);
 
             var userData = this.service.login(data.getEmailId(), data.getPassword());
             if(userData != null){
@@ -97,13 +104,11 @@ public class userController {
             }
             //System-log
             System.out.println("Login Successful");
-            status = HttpStatus.OK;
         }
         catch (Exception ex){
             //System-log
             System.out.println("Exception at login" + ex);
         }
-
-        return new ResponseEntity<>(response, status);
+        producer.send(loginResponse, response);
     }
 }
