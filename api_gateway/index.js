6bc7b412367ff9a6cf49926193fafd2a8121cac2
Amber Ramesh <amramesh@iu.edu>
2022-04-07 04:14:01 -0400

    (#76) Add response streaming to gateway - Add updated endpoint for streaming NEXRAD ingestor response - Add memorystream package for creating writeable streams - Update Kafka promise handlers - Remove defunct API handlers

--- api_gateway/index.js
+++ api_gateway/index.js
@@ -3,38 +3,38 @@ const app = express()
 const server = require('http').createServer(app)
 const cors = require('cors')
 const bodyParser = require('body-parser')
-const kafka = require("kafka-node");
+const kafka = require('kafka-node')
+const MemoryStream = require('memorystream')
+const { finished } = require('stream/promises')
 app.use(cors())
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-const { identityApi, historyApi, ingestorApi } = require('./config/api')
+const { nexradApi } = require('./config/api')
 const logger = require('./config/logging')
 const config = require('./config/appconfig')
 
-const requestForwarder = async function (apiInstance, endpoint, requestBody) {
-  logger.debug(`POST ${apiInstance.defaults.baseURL}${endpoint} ${JSON.stringify(requestBody) || '""'}`)
+const requestForwarder = async function (apiInstance, endpoint, requestBody, config = {}) {
+  logger.debug(`POST ${apiInstance.defaults.baseURL}${endpoint} ${JSON.stringify(requestBody)}`)
   try {
-    const response = await apiInstance.post(endpoint, requestBody)
-    return response.data
+    const response = await apiInstance.post(endpoint, requestBody, config)
+    return response
   } catch (err) {
     logger.error(err.message)
     throw err
   }
 }
 
-const asyncForwarder = async function (req, apiInstance, endpoint = '') {
-  const data = req.body
+const streamingForwarder = async function (req, res, apiInstance, endpoint = '') {
+  const requestData = req.body
   try {
-    return await requestForwarder(apiInstance, endpoint, data)
-  } catch (err) { }
-}
-
-const standardForwarder = async function (req, res, apiInstance, endpoint = '') {
-  const data = req.body
-  try {
-    const result = await requestForwarder(apiInstance, endpoint, data)
-    res.status(200).json(result)
+    const responseStream = new MemoryStream()
+    const { data, headers } = await requestForwarder(apiInstance, endpoint, requestData, { responseType: 'stream' })
+    res.setHeader('Content-Type', headers['content-type'])
+    res.setHeader('Content-Disposition', headers['content-disposition'])
+    data.pipe(res)
+    await finished(responseStream)
+    res.end()
   } catch (err) {
     res.status(500).send(err.message)
   }
@@ -42,7 +42,6 @@ const standardForwarder = async function (req, res, apiInstance, endpoint = '') 
 
 const kafkaHandler = async function (reqTopic, resTopic, request, response = null) {
   const data = JSON.stringify(request.body)
-  
   let payloads = [
     {
       topic: reqTopic,
@@ -51,102 +50,76 @@ const kafkaHandler = async function (reqTopic, resTopic, request, response = nul
   ];
 
   try {
-    const messageValue = await new Promise((resolve, reject) => {
+    const message = await new Promise((resolve, reject) => {
 
-      client = new kafka.KafkaClient({kafkaHost:config.kafkaBootstrapServer})
-      
+      client = new kafka.KafkaClient({ kafkaHost: config.kafkaBootstrapServer })
       producer = new kafka.Producer(client)
 
-      producer.send(payloads, (err) => {
-        if (err) {
-          logger.error(err)
-        }
-      });
-
-      consumer = new kafka.Consumer(client, [ 
-        { 
-            topic: resTopic, 
-            partition: 0 
-        } ], 
-        { 
-            autoCommit: false 
+      producer.send(payloads, reject);
+      consumer = new kafka.Consumer(client, [
+        {
+          topic: resTopic,
+          partition: 0
+        }],
+        {
+          autoCommit: false
         });
-        
-      consumer.on('message', function (message) {
-        resolve(message.value)
-      });
 
-      consumer.on('error', function (err) {
-        reject(err)
-      })
+      consumer.on('message', resolve);
+      consumer.on('error', reject)
     })
 
-    logger.debug(messageValue)
+    // Extract only value property of the consumed message
+    const { value } = message
+    logger.debug(value)
 
-    if(response != null){
-      response.status(200).send(messageValue)
+    if (!!response) {
+      response.status(200).send(value)
     }
   } catch (err) {
     logger.error(err)
-    response.status(500).send('An error occurred.')
+    if (!!response) {
+      response.status(500).send('An error occurred.')
+    }
   }
 }
 
-// Register - REST based
-// app.post('/register', (req, res) => standardForwarder(req, res, identityApi, '/register'))
-
 //Register - KAFKA based
-app.post('/register', (req, res) => 
-kafkaHandler(
-  config.topic_user_register_request, 
-  config.topic_user_register_response,
-  req, 
-  res
+app.post('/register', (req, res) =>
+  kafkaHandler(
+    config.topic_user_register_request,
+    config.topic_user_register_response,
+    req,
+    res
   )
 )
-
-// Login
-// app.post('/login', (req, res) => standardForwarder(req, res, identityApi, '/login'))
 
 // Login - KAFKA based
-app.post('/login', (req, res) => 
-kafkaHandler(
-  config.topic_user_login_request, 
-  config.topic_user_login_response,
-  req, 
-  res 
+app.post('/login', (req, res) =>
+  kafkaHandler(
+    config.topic_user_login_request,
+    config.topic_user_login_response,
+    req,
+    res
   )
 )
-// app.post('/login', (req, res) => kafkaHandler(config.topic_user_login_request, req, res, login_response))
 
-// User-History + (Ingestor - Storm Clustering - Forecast)
-app.post('/forecast', (req, res) => {
-  asyncForwarder(req, historyApi, 'save')
+// User-History + NEXRAD Ingestor
+app.post('/weather/radar', (req, res) => {
   kafkaHandler(
-    config.topic_user_history_save_request, 
+    config.topic_user_history_save_request,
     config.topic_user_history_save_response,
     req)
-  standardForwarder(req, res, ingestorApi)
+  streamingForwarder(req, res, nexradApi)
 })
 
-// app.get('/history', async (req, res) => {
-//   try {
-//     const data = req.body
-//     const response = await historyApi.post('get', data)
-//     return res.status(200).json(response.data)
-//   } catch (err) {
-//     logger.error(err.message)
-//     res.status(400).json(err.message)
-//   }
-// })
-
 // History get - KAFKA based
-app.get('/history', (req, res) => 
-kafkaHandler(
-  config.topic_user_history_get_request, 
-  config.topic_user_history_get_response,
-  req, 
-  res 
+app.get('/history', (req, res) =>
+  kafkaHandler(
+    config.topic_user_history_get_request,
+    config.topic_user_history_get_response,
+    req,
+    res
   )
 )
 
