174bc96db2c6bf73aad30b1307e0a91335c7fdd3
Amber Ramesh <amramesh@iu.edu>
2022-04-06 20:34:59 -0400

    (#75) Switch from snake case params to camel case

--- ingestor_service/models.py
+++ ingestor_service/models.py
@@ -1,5 +1,10 @@
+from ast import alias
 from datetime import datetime
 from pydantic import BaseModel
+
+
+def to_camel(string: str) -> str:
+    return ''.join(word.capitalize() if index else word for index, word in enumerate(string.split('_')))
 
 
 class RadarRequest(BaseModel):
@@ -7,3 +12,6 @@ class RadarRequest(BaseModel):
     end_time: datetime
     longitude: float
     lattitude: float
+
+    class Config:
+        alias_generator = to_camel
