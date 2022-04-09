03c0b7a8dea74131fabd03aea2b3835aa6be6725
Amber Ramesh <amramesh@iu.edu>
2022-04-09 05:21:40 -0400

    (#75) Update run.sh for building new service

--- ingestor_service/run.sh
+++ ingestor_service/run.sh
@@ -1,2 +1,4 @@
-docker build . --no-cache -t vulcans/nexrad-ingestor-service &&
+docker stop nexrad-ingestor-service
+docker rmi vulcans/nexrad-ingestor-service
+docker build --no-cache -t vulcans/nexrad-ingestor-service . &&
 docker-compose up
